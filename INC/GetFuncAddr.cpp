a7cfdd40cf76d237aade2a47f830da6a208bcb13
rbmm <_dm_sh_@tutanota.com>
2024-02-29 15:19:53 +0200

    Update GetFuncAddr.cpp

--- INC/GetFuncAddr.cpp
+++ INC/GetFuncAddr.cpp
@@ -8,72 +8,72 @@ _NT_BEGIN
 #include "../inc/asmfunc.h"
 
 char __fastcall fmemcmp(
-						const void *buf1,
-						const void *buf2,
-						size_t count
-						)ASM_FUNCTION;
+            const void *buf1,
+            const void *buf2,
+            size_t count
+            )ASM_FUNCTION;
 
 PVOID __fastcall get_hmod(PCWSTR lpModuleName)
 {
-	CPP_FUNCTION;
+  CPP_FUNCTION;
 
-	if (!*lpModuleName)
-	{
-		return CONTAINING_RECORD(((NT::_TEB*)NtCurrentTeb())->ProcessEnvironmentBlock->Ldr->InMemoryOrderModuleList.Flink->Flink, 
-			_LDR_DATA_TABLE_ENTRY, InMemoryOrderLinks)->DllBase;
-	}
+  if (!*lpModuleName)
+  {
+    return CONTAINING_RECORD(((NT::_TEB*)NtCurrentTeb())->ProcessEnvironmentBlock->Ldr->InMemoryOrderModuleList.Flink->Flink, 
+      _LDR_DATA_TABLE_ENTRY, InMemoryOrderLinks)->DllBase;
+  }
 
-	HMODULE hmod;
-	UNICODE_STRING DllName = { 
-		(USHORT)wcslen(lpModuleName) * sizeof(WCHAR), 
-		DllName.Length, 
-		const_cast<PWSTR>(lpModuleName) 
-	};
-	if (0 > LdrLoadDll(0, 0, &DllName, &hmod)) __debugbreak();
-	return hmod;
+  HMODULE hmod;
+  UNICODE_STRING DllName = { 
+    (USHORT)wcslen(lpModuleName) * sizeof(WCHAR), 
+    DllName.Length, 
+    const_cast<PWSTR>(lpModuleName) 
+  };
+  if (0 > LdrLoadDll(0, 0, &DllName, &hmod)) __debugbreak();
+  return hmod;
 }
 
 PVOID __fastcall GetFuncAddressEx(PIMAGE_DOS_HEADER pidh, PCSTR lpsz)
 {
-	CPP_FUNCTION;
+  CPP_FUNCTION;
 
-	PIMAGE_NT_HEADERS pinth = (PIMAGE_NT_HEADERS)RtlOffsetToPointer(pidh, pidh->e_lfanew);
+  PIMAGE_NT_HEADERS pinth = (PIMAGE_NT_HEADERS)RtlOffsetToPointer(pidh, pidh->e_lfanew);
 
-	PIMAGE_EXPORT_DIRECTORY pied = (PIMAGE_EXPORT_DIRECTORY)RtlOffsetToPointer(pidh, 
-		pinth->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_EXPORT].VirtualAddress);
+  PIMAGE_EXPORT_DIRECTORY pied = (PIMAGE_EXPORT_DIRECTORY)RtlOffsetToPointer(pidh, 
+    pinth->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_EXPORT].VirtualAddress);
 
-	PDWORD AddressOfNames = (PDWORD)RtlOffsetToPointer(pidh, pied->AddressOfNames);
-	PDWORD AddressOfFunctions = (PDWORD)RtlOffsetToPointer(pidh, pied->AddressOfFunctions);
-	PWORD AddressOfNameOrdinals = (PWORD)RtlOffsetToPointer(pidh, pied->AddressOfNameOrdinals);
+  PDWORD AddressOfNames = (PDWORD)RtlOffsetToPointer(pidh, pied->AddressOfNames);
+  PDWORD AddressOfFunctions = (PDWORD)RtlOffsetToPointer(pidh, pied->AddressOfFunctions);
+  PWORD AddressOfNameOrdinals = (PWORD)RtlOffsetToPointer(pidh, pied->AddressOfNameOrdinals);
 
-	DWORD a = 0, b = pied->NumberOfNames, o;
+  DWORD a = 0, b = pied->NumberOfNames, o;
 
-	SIZE_T len = strlen(lpsz) + 1;
+  SIZE_T len = strlen(lpsz) + 1;
 
-	if (b) 
-	{
-		do
-		{
-			char i = fmemcmp(lpsz, RtlOffsetToPointer(pidh, AddressOfNames[o = (a + b) >> 1]), len);
-			if (!i)
-			{
-				PVOID pv = RtlOffsetToPointer(pidh, AddressOfFunctions[AddressOfNameOrdinals[o]]);
+  if (b) 
+  {
+    do
+    {
+      char i = fmemcmp(lpsz, RtlOffsetToPointer(pidh, AddressOfNames[o = (a + b) >> 1]), len);
+      if (!i)
+      {
+        PVOID pv = RtlOffsetToPointer(pidh, AddressOfFunctions[AddressOfNameOrdinals[o]]);
 
-				if ((ULONG_PTR)pv - (ULONG_PTR)pied < pinth->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_EXPORT].Size)
-				{
-					ANSI_STRING as = { (USHORT)len, as.Length, const_cast<PSTR>(lpsz) };
-					if (0 > LdrGetProcedureAddress((HMODULE)pidh, &as, 0, &pv)) return 0;
-				}
+        if ((ULONG_PTR)pv - (ULONG_PTR)pied < pinth->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_EXPORT].Size)
+        {
+          ANSI_STRING as = { (USHORT)len-1, as.Length, const_cast<PSTR>(lpsz) };
+          if (0 > LdrGetProcedureAddress((HMODULE)pidh, &as, 0, &pv)) return 0;
+        }
 
-				return pv;
-			}
+        return pv;
+      }
 
-			if (0 > i) b = o; else a = o + 1;
+      if (0 > i) b = o; else a = o + 1;
 
-		} while (a < b);
-	}
+    } while (a < b);
+  }
 
-	return 0;
+  return 0;
 }
 
 _NT_END
